7814d884cbd36ec98199ff74a8481b0c4e984a47
Felix <felixhultin@yahoo.se>
2024-04-12 16:32:43 +0200

    feat: more work on (unfinished) main script

--- sb2hf.py
+++ sb2hf.py
@@ -7,16 +7,31 @@ import json
 import os
 import shutil
 
-from hf_gen.generate_config import generate_config
+from urlparser import URLReader
+
+
+def write_repository(url_reader):
+    output_folder = f"{url_reader.resource_name}"
+    print(f"Saving repository to {output_folder}")
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
+    os.mkdir(output_folder)
+    url_reader.download_file(to=output_folder)
+    with open(f'{output_folder}/{url_reader.resource_name}_config.json', 'w') as f:
+        config = {
+            'url': url_reader.url,
+            'homepage': url_reader.url,
+            'resource_name': url_reader.resource_name,
+            'description': None,
+            'citation': None
+        }
+        json.dump(config, f, indent=4, sort_keys=True)
+        shutil.copyfile('hf_gen/dataset_loading_script.py', f'{output_folder}/{url_reader.resource_name}.py')
+
 
 def sb2hf():
-    output_folder = f"generated/{args.hf_output_folder}"
-    print(f"Saving repository to {output_folder}")
-    os.mkdir(output_folder)
-    with open('sb_config.json', 'w') as f:
-        config = generate_config(args.resource_url, args.row_output)
-        json.dump(config, f)
-        shutil.copyfile('hf_gen/dataset_loading_script.py', f'{output_folder}/dataset_loading_script.py')
+    url_reader = URLReader(args.url)
+    write_repository(url_reader)    
     print("Finished. Now you just need to commit the repository to HuggingFace.")
     # TODO Create repository on HuggingFace programatically.
 
@@ -24,13 +39,16 @@ def sb2hf():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(            
         prog='sb2hf',
-        description='Creates an HuggingFace repository from an existing SB resource')
+        description='Creates a HuggingFace repository from an existing SB resource')
     parser.add_argument('url', help='URL to SB resource file (usually xml)')
     parser.add_argument('--hf-output-folder', help='Where to locally save the resulting HuggingFace repository.')
+    parser.add_argument('--push-to-hub', help='If activated, pushes generated repository directly to hub.')
     parser.add_argument(
         '-r', '--row-output',
         choices=['sentences', 'tokens'], 
         help='Which output the rows should be in.'
     )
     args = parser.parse_args()
-    sb2hf()
+    if args.hf_output_folder is None:
+        args.hf_output_folder = args.url.split('/')[-1]
+    test = sb2hf()
