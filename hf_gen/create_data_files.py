0ccd0ae636ff5825a418a9f71427c69e9cd3d0d4
Felix <felixhultin@yahoo.se>
2025-02-12 11:42:22 +0100

    remove xml file after download

--- hf_gen/create_data_files.py
+++ hf_gen/create_data_files.py
@@ -16,6 +16,7 @@ def write_files(config):
         output_folder = config.output_folder
         output_path = f'{output_folder}/all.tsv'
         _write_file(config.bz2_local_path, output_path)
+        Path(config.bz2_local_path).unlink()
     else:
         for fn in config.sbx2hf_args['paths']:
             assert fn.endswith('.xml.bz2') or fn.endswith('.xml')
